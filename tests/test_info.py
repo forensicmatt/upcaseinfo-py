9d5e0f33ac47ab837f66ee1b3bae40ed09a708c3
forensicmatt <matthew.seyer@gmail.com>
2020-07-12 15:28:17 -0600

    added build to test

--- tests/test_info.py
+++ tests/test_info.py
@@ -20,6 +20,7 @@ class TestUpcaseInfo(unittest.TestCase):
         self.assertEqual(15770619046507800844, upcaseinfo.crc)
         self.assertEqual(10, upcaseinfo.osmajor)
         self.assertEqual(0, upcaseinfo.osminor)
+        self.assertEqual(16299, upcaseinfo.build)
         self.assertEqual(0, upcaseinfo.packmajor)
         self.assertEqual(0, upcaseinfo.packminor)
 
